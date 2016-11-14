365f9ed75a72262b743e8ba263b281e3e0ab9146
Steve <steven.grain@wearecube3.com>
2016-11-14 15:23:02 +0000

    Removed backup themes

--- web/app/themes/pedal-devil-170716/template-contact.php
+++
@@ -1,8 +0,0 @@
-<?php
-/*
- * Template Name: Contact
-*/
-$context = Timber::get_context();
-$post = new TimberPost();
-$context['post'] = $post;
-Timber::render( 'page-templates/contact.twig', $context );
